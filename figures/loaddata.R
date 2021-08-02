190a9cbe3608dbcf51343f9e425baf8fa5880b65
Abby <aebratt@uw.edu>
2021-08-02 10:30:26 -0700

    fix data process script

--- figures/loaddata.R
+++ figures/loaddata.R
@@ -9,12 +9,35 @@ library(coda)
 library(nlist)
 library(beepr)
 
-setwd("/Users/aebratt/Desktop/IPMEURING")
+scenarios <- readRDS(here("data", "scenarios.RDS"))
+low.lam.combos <- readRDS(here("data","low.lam.params.RDS"))
+med.lam.combos <- readRDS(here("data","med.lam.params.RDS"))
+high.lam.combos <- readRDS(here("data","high.lam.params.RDS"))
+
+# functions
+source(here("scripts", "current version",
+            "0 - preparing scenarios", "compute_time_calc.R"))
+
+# determine priority score for scenarios
+scenarios %>% mutate(priority = NA_integer_)
+for (i in 1:nrow(scenarios)) {
+  tmp <- scenarios[i, 1:3]
+  tmp <- tmp[!is.na(tmp)]
+  scenarios[i, "priority"] <- length(unique(tmp))
+}
+scenarios <- scenarios %>% arrange(priority) # save in prioritized order
+
+# set working directory
+setwd("D:/all_results")
+
+scenarios.picked <- 25
+sims.per <- 25
+scenario <- dim(scenarios)[1]
 
 # SLOW TO RUN BUT DOES RUN
-for (i in 1:10) { #scenarios picked
-  for (j in 1:25) { # sims per
-    for (k in 1:9) { # simulation scenario
+for (i in 1:scenarios.picked) { #scenarios picked
+  for (j in 1:sims.per) { # sims per
+    for (k in 1:scenario) { # simulation scenario
       print(paste(i, j, k), sep = " ")
       if (k %in% 1:3) {
         out <- readRDS(paste("lowout", "-", i, "-", j, "-", k, ".RDS", sep = ""))
@@ -63,38 +86,6 @@ for (i in 1:10) { #scenarios picked
   }
 }
 
-# presumably all of these are good to go, so let's collapse the chains 
-# and thin the
-
 rm(list=grep("highout|medout|lowout",ls(),value=TRUE,invert=TRUE))
 save.image("processedIPMOutput.RData")
-
-# repeat this for case study #####
-
 rm(list = ls())
-setwd("/Users/aebratt/Desktop/OOPSIPM")
-
-# SLOW TO RUN BUT DOES RUN
-for (i in 1:10) { #scenarios picked
-  for (j in 1:25) { # sims per
-    for (k in 1:9) { # simulation scenario
-      print(paste(i, j, k), sep = " ")
-        out <- readRDS(paste("lowout", "-", i, "-", j, "-", k, ".RDS", sep = ""))
-        tmp <- max(gelman.diag(out, multivariate = FALSE)[[1]][, 1])
-        if (!is.na(tmp) & tmp <= 1.2) {
-          out <- out %>% 
-            collapse_chains() %>% 
-            as.matrix() %>% 
-            as.data.frame() %>% 
-            filter(row_number() %% 60 == 1) %>% 
-            mutate(scenario= i) %>% 
-            mutate(sims = j) %>% 
-            mutate(simscenarios = k)
-          assign(paste("lowout", "-", i, "-", j, "-", k, sep = ""), out)
-        }
-    }
-  }
-}
-
-rm(list=grep("highout|medout|lowout",ls(),value=TRUE,invert=TRUE))
-save.image("processedIPMcasestudy.RData")
